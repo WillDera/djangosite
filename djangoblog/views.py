4d7fc11e0ed326a4a4e90a197d14b55be53c2917
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-02 14:52:19 +0100

    Everything works

--- djangoblog/views.py
+++ djangoblog/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
 from django.http import HttpResponse
-from django.template import loader
+
 
 def index(request):
-    template = loader.get_template('djangoblog/homepage.html')
-    return HttpResponse(template)
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def about(request):
-    template = loader.get_template('djangoblog/about.html')
-    return HttpResponse(template)
+    return HttpResponse("Hello, world. You're at the polls index.")
