7be563a95b30c6e05999aa966fd2025d0f012d3a
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-02 18:55:53 +0100

    Created the database model for the blog.

--- djangoblog/views.py
+++ djangoblog/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-
 
 def index(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'homepage.html')
+    return render(request, 'djangoblog/homepage.html')
 
 def about(request):
     # return HttpResponse("Hello, world. You're at the polls index.")
-    return render(request, 'about.html')
+    return render(request, 'djangoblog/about.html')
