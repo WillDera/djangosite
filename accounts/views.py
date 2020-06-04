e4cbf90721ec8909778dcb0bbd1707a84be14cbc
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-04 23:53:56 +0100

    User LogIn and Validation.
    Login authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
@@ -21,3 +21,13 @@ def signup_view(request):
     # the 3rd param in render is the data being passed down to 
     # the template "signup.html"
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            # log user in
+            return redirect('djangoblog:article')
+    else:
+        form = AuthenticationForm
+    return render(request, 'accounts/login.html', {'form':form})
