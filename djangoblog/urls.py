1ede3ddebe557c319f31eadabd9c705a0a1a5c6c
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-06 21:26:47 +0100

    Fixed Routing 🚀
    Base "/" Route sends to article list.

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'djangoblog'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.article, name='index'),
     path('article/', views.article, name='article'),
     path('article/create/', views.article_create, name='create'),
     path('article/<slug>/', views.article_detail, name='article_detail'),
