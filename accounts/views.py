542f37acee50ecc9e2e015fa975029f1be0d7c59
Godswill <30760648+WillDera@users.noreply.github.com>
2020-06-28 08:24:02 +0100

    Done 🏂

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,10 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 
 # Create your views here.
+
+
 def signup_view(request):
-    
+
     # check if request is a POST
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -21,9 +23,10 @@ def signup_view(request):
         # create new instance of the UserCreationForm
         form = UserCreationForm()
 
-    # the 3rd param in render is the data being passed down to 
+    # the 3rd param in render is the data being passed down to
     # the template "signup.html"
     return render(request, 'accounts/signup.html', {'form': form})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -33,17 +36,17 @@ def login_view(request):
             user = form.get_user()
             # log the user in
             login(request, user)
-            
+
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
             else:
                 return redirect('djangoblog:article')
     else:
         form = AuthenticationForm
-    return render(request, 'accounts/login.html', {'form':form})
+    return render(request, 'accounts/login.html', {'form': form})
+
 
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('djangoblog:article')
-
